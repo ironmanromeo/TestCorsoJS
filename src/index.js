107c20992a5dfc5f733603349c72568d5fcd6137
Romeo Mariani <romeo.mariani@valxer.it>
2019-11-03 14:45:50 +0100

    Commit 3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla! Romeo</h1>
 <div>
-  Commit 2: We useffff Parcel to bundle this sandbox, you can find more info about Parcel
+  Commit 3: We useffff Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
